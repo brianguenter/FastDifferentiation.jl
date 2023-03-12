357f490db271831e3ec3bf25934d43063d99de7f
brianguenter <1brianguenter@gmail.com>
2023-03-12 11:14:24 -0700

    deleted set_zero! not used anywhere

--- src/BitVectorFunctions.jl
+++ src/BitVectorFunctions.jl
@@ -1,13 +1,10 @@
-"""returns true if a ⊆ b, false otherwise"""
+#These functions appear to be """returns true if a ⊆ b, false otherwise"""
 subset(a::BitVector, b::BitVector) = sum(a .& b) == sum(a)
 export subset
 
 """returns the set that has all the elements of b removed from a"""
 set_diff(a::BitVector, b::BitVector) = @. !(a & b) & a
 export set_diff
-
-set_zero!(a::BitVector) = @. a = a ⊻ a
-export zero_out
 
 bit_equal(a::BitVector, b::BitVector) = !any(a .⊻ b)
 export bit_equal
