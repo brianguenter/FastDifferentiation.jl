6bcd82fe92bab4a30d15fbd0e76ec139fec185d1
brianguenter <1brianguenter@gmail.com>
2023-03-21 16:53:31 -0700

    restored accidentally code in FSDTests compute_factorable_subgraphs test order

--- src/FSDTests.jl
+++ src/FSDTests.jl
@@ -256,6 +256,15 @@ end
     n3 = nv1 * nv2
     n4 = n3 * nv1
 
+    n5 = n3 * n4
+
+    graph = DerivativeGraph([n4, n5])
+    # factor_subgraph!(graph, postdominator_subgraph(2, 4, 2, BitVector([0, 1]), BitVector([0, 1])))
+    subs, _ = compute_factorable_subgraphs(graph)
+
+    _5_3 = dominator_subgraph(graph, 5, 3, Bool[0, 1], Bool[0, 1], Bool[1, 1])
+    _1_4 = postdominator_subgraph(graph, 1, 4, Bool[1, 0], Bool[1, 1], Bool[1, 0])
+    _3_5 = postdominator_subgraph(graph, 3, 5, Bool[0, 1], Bool[0, 1], Bool[1, 1])
     _4_1 = dominator_subgraph(graph, 4, 1, Bool[1, 0], Bool[1, 1], Bool[1, 0])
     _5_1 = dominator_subgraph(graph, 5, 1, Bool[0, 1], Bool[0, 1], Bool[1, 0])
     _1_5 = postdominator_subgraph(graph, 1, 5, Bool[1, 0], Bool[0, 1], Bool[1, 0])
