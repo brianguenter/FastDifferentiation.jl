cdfe197a631129cd5b0fb27aadbaaf124dbba846
BrianGuenter <1brianguenter@gmail.com>
2023-05-31 10:18:59 -0700

    removed all references to Symbolics so they can take a dependency on FSD.

--- src/ExpressionGraph.jl
+++ src/ExpressionGraph.jl
@@ -84,11 +84,10 @@ is_leaf(::Node{T,N}) where {T,N} = false
 is_tree(::Node{T,N}) where {T,N} = N >= 1
 
 
-# is_variable(a::Node) = SymbolicUtils.issym(value(a))
 is_variable(a::Node) = isa(value(a), Symbol)
 
 
-is_constant(a::Node) = !is_variable(a) && !is_tree(a) #pretty confident this is correct but there may be edges cases in Symbolics I am not aware of.
+is_constant(a::Node) = !is_variable(a) && !is_tree(a)
 
 
 function constant_value(a::Node)
@@ -103,7 +102,7 @@ end
 function is_zero(a::Node)
     if is_tree(a) || is_variable(a)
         return false
-    elseif value(a) == 0 #may think could just do this test but in Symbolics x==0 x*x == 0 is a non-boolean expression.
+    elseif value(a) == 0
         return true
     else
         return false
@@ -114,7 +113,7 @@ end
 function is_one(a::Node)
     if is_tree(a) || is_variable(a)
         return false
-    elseif value(a) == 1 #may think could just do this test but in Symbolics x==1 or x*x == 1 is a non-boolean expression.
+    elseif value(a) == 1
         return true
     else
         return false
@@ -354,76 +353,11 @@ function to_string(a::Node)
     end
 end
 
-expr_to_dag(x::AutomaticDifferentiation.NoDeriv, cache, substitions) = Node(NaN) #when taking the derivative with respect to the first element of 1.0*x Symbolics.derivative will return Symbolics.NoDeriv. These derivative values will never be used (or should never be used) in my derivative computation so set to NaN so error will show up if this ever happens.
-
-function expr_to_dag(x::Real, cache::IdDict=IdDict(), substitutions::Union{IdDict,Nothing}=nothing)
-    return _expr_to_dag(x, cache, substitutions)
-end
-export expr_to_dag
-
-
-#WARNING!!!!!!! TODO. *,+ simplification code relies on Node(0) have node_value 0 as Int64. Need to make sure that expr_to_dag unwraps numbers or the simplification code won't work.
-function _expr_to_dag(symx, cache::IdDict, substitutions::Union{IdDict,Nothing}) #cache is an IdDict, to make clear that hashing into the cache Dict is  based on objectid, i.e., using === rather than ==.
-    # Substitutions are done on a Node graph, not a SymbolicsUtils.Sym graph. As a consequence the values
-    # in substitutions Dict are Node not Sym type. cache has keys (op,args...) where op is generally a function type but sometimes a Sym, 
-    # and args are all Node types.
-
-    #need to extract the SymbolicUtils tree from symx
-
-    if isa(symx, Num) #substitutions are stored as SymbolicUtils.Symx so extract the underlying Symx value
-        symx = symx.val
-    end
-
-
-    if substitutions !== nothing
-
-        tmpsub = get(substitutions, symx, nothing)
-        if tmpsub !== nothing
-            return substitutions[symx] #substitute Node object for symbolic object created in differentiation
-        end
-    end
-
-    tmp = get(cache, symx, nothing)
-
-    if tmp !== nothing
-        return tmp
-    elseif !SymbolicUtils.istree(symx)
-
-        tmpnode = Node(symx)
-        cache[symx] = tmpnode
-
-        return tmpnode
-    else
-        numargs = length(SymbolicUtils.arguments(symx))
-        symargs = MVector{numargs}(SymbolicUtils.arguments(symx))
-
-
-        #Taking Ref(nothing) causes the broadcasting to screw up for some reason. need two cases on for subsitutions === nothing and one for it being an IdDict.
-        if substitutions === nothing
-            args = _expr_to_dag.(symargs, Ref(cache), substitutions)
-        else
-            args = _expr_to_dag.(symargs, Ref(cache), Ref(substitutions))
-        end
-
-        key = (SymbolicUtils.operation(symx), args...)
-        tmp = get(cache, key, nothing)
-
-        if tmp !== nothing
-            return tmp
-        else
-            tmpnode = Node(SymbolicUtils.operation(symx), args)
-            cache[key] = tmpnode
-
-            return tmpnode
-        end
-    end
-end
-
 function node_symbol(a::Node, variable_to_index::Dict{Node,Int64})
     if is_tree(a)
         result = gensym() #create a symbol to represent the node
     elseif is_variable(a)
-        result = :(input_variables[$(variable_to_index[a])])  #use the name of the Symbolics symbol which represents the variable
+        result = :(input_variables[$(variable_to_index[a])])
     else
         result = value(a) #not a tree not a variable so is some kind of constant.
     end
@@ -473,20 +407,6 @@ function function_body!(dag::Node, variable_to_index::Dict{Node,Int64}, node_to_
 
     return body, _dag_to_function(dag)
 end
-
-"""converts from dag to Symbolics expression"""
-function to_symbolics(a::Node)
-    if arity(a) === 0
-        return Num(value(a)) #convert everything to Num type. This will wrap types like Int64,Float64, etc., but will not double wrap nodes that are Num types already.
-    else
-        if arity(a) === 1
-            return a.node_value(to_symbolics(a.children[1]))
-        else
-            return foldl(a.node_value, to_symbolics.(a.children))
-        end
-    end
-end
-export to_symbolics
 
 """Used to postorder function with multiple outputs"""
 function postorder(roots::AbstractVector{T}) where {T<:Node}
--- src/FSDTests.jl
+++ src/FSDTests.jl
@@ -50,17 +50,6 @@ function number_of_operations(symbolic_expr)
     end
 end
 
-function testtest()
-    order = 10
-
-    fsd_graph = spherical_harmonics(FastSymbolic(), order)
-    fsd_func = roots(fsd_graph)
-    func_vars = variables(fsd_graph)
-
-    Jv, v_vars = jacobian_times_v(fsd_func, func_vars)
-end
-export testtest
-
 function simple_dag(cache::Union{IdDict,Nothing}=IdDict())
     @variables zz y
     return expr_to_dag(zz^2 + y * (zz^2), cache), zz, y
@@ -348,41 +337,6 @@ end
 end
 
 
-#TODO conversion 
-# @testitem "all_nodes" begin
-#     using FastSymbolicDifferentiation.FSDTests
-#     using FastSymbolicDifferentiation.FSDInternals
-
-#     cache = IdDict()
-#     dag, x, y = simple_dag(cache)
-
-#     correct = expr_to_dag.([((x^2) + (y * (x^2))), (x^2), x, 2, (y * (x^2)), y], Ref(cache))
-#     tmp = all_nodes(dag)
-
-#     #verify that all the node expressions exist in the dag. Can't rely on them being in a particular order because Symbolics can
-#     #arbitrarily choose how to reorder trees.
-#     for expr in correct
-#         @test in(expr, tmp)
-#     end
-# end
-
-
-#TODO conversion 
-# @testitem "conversion from graph of FastSymbolicDifferentiation.Node to Symbolics expression" begin
-#     using FastSymbolicDifferentiation.FSDTests
-#     import Symbolics
-
-#     order = 7
-#     @variables x y z
-
-#     derivs = Symbolics.jacobian(SHFunctions(order, x, y, z), [x, y, z]; simplify=true)
-#     # show(@time SHDerivatives(order,x,y,z))
-#     tmp = expr_to_dag.(derivs)
-#     # show(@time expr_to_dag.(derivs))
-#     from_dag = to_symbolics.(tmp)
-#     subs = Dict([x => rand(), y => rand(), z => rand()])
-#     @test isapprox(map(xx -> xx.val, Symbolics.substitute.(derivs, Ref(subs))), map(xx -> xx.val, Symbolics.substitute.(from_dag, Ref(subs))), atol=1e-12)
-# end
 
 @testitem "is_tree" begin
     using FastSymbolicDifferentiation.FSDInternals
