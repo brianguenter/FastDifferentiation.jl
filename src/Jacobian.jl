fafd83b4328f3fad6e6551f3b8a1f6f8f7a6d602
brianguenter <116222085+brianguenter@users.noreply.github.com>
2023-06-13 18:31:36 -0700

    Update Jacobian.jl doc comment

--- src/Jacobian.jl
+++ src/Jacobian.jl
@@ -264,17 +264,17 @@ export sparse_hessian
 
 ```julia_repl
 
-julia> A = [t t^2;3t^2 5]
+julia> A = [t t^2;3t^2 5]  
 2×2 Matrix{Node}:
  t              (t ^ 2)
  (3 * (t ^ 2))  5
 
-julia> derivative(A,t)
+julia> derivative(A,t)  
 2×2 Matrix{Node}:
  1.0      (2 * t)
  (6 * t)  0.0
 
- julia> derivative(A,t,t)
+ julia> derivative(A,t,t)  
  2×2 Matrix{Node{T, 0} where T}:
  0.0  2
  6    0.0
