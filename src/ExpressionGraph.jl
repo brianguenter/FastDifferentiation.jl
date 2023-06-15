c5de3242d598a5626901cca8afc59ae3625f6bac
brianguenter <1brianguenter@gmail.com>
2023-06-15 08:46:55 -0700

    sp. correction in doc comment

--- src/ExpressionGraph.jl
+++ src/ExpressionGraph.jl
@@ -12,7 +12,7 @@ function check_cache(a::Tuple{Vararg}, cache)
     return cache[a]
 end
 
-"""Clears the global expression cache. To maximize efficiency of expressions the differentation system automatically eliminates common subexpressions by checking for there existence in the global expression cache. Over time this cache can become arbitrarily large. Best practice is to clear the cache before you start defining expressions, define your expressions and then clear the cache."""
+"""Clears the global expression cache. To maximize efficiency of expressions the differentation system automatically eliminates common subexpressions by checking for their existence in the global expression cache. Over time this cache can become arbitrarily large. Best practice is to clear the cache before you start defining expressions, define your expressions and then clear the cache."""
 clear_cache() = empty!(EXPRESSION_CACHE)
 export clear_cache
 
