574055cea07156e2694e0f49123523edcd2e4ae9
brianguenter <1brianguenter@gmail.com>
2023-07-07 11:11:31 -0700

    fixes onunit conversion bug so static array constructor won't bomb.

--- src/FastDifferentiation.jl
+++ src/FastDifferentiation.jl
@@ -51,21 +51,6 @@ function draw_dot end
 function write_dot end
 
 
-function test()
-    x, graph, _, _ = simple_dominator_graph()
-
-    factor!(graph)
-    fedge = edges(graph, 1, 4)[1]
-    tmp0 = make_function([value(fedge)], [x])
-    dfsimp(x) = tmp0([x])[1]
-    x, graph, _, _ = simple_dominator_graph() #x is a new variable so have to make a new Node(x)
-
-    tmp00 = make_function([root(graph, 1)], [x])
-    origfsimp(x) = tmp00([x])[1]
-    @assert isapprox(FiniteDifferences.central_fdm(5, 1)(origfsimp, 3), dfsimp(3)[1])
-end
-
-include("FDTests.jl")
 
 
 end # module
