f3f477fcd4cd0f9f89eb141e2a4aa25dc7ccaab5
brianguenter <1brianguenter@gmail.com>
2023-07-25 15:56:40 -0700

    added differential operator

--- src/Jacobian.jl
+++ src/Jacobian.jl
@@ -348,3 +348,29 @@ function _derivative(A::AbstractArray{<:Node}, variable::T) where {T<:Node}
         return Node.(result)
     end
 end
+
+"""Returns an anonymous function that takes the derivative of a scalar function with respect to `variables`. Example:
+```julia
+julia> @variables t
+t
+
+julia> f = t^2
+(t ^ 2)
+
+julia> Dt = differential(t)      
+#69 (generic function with 1 method)
+
+julia> Dt(f)
+(2 * t)
+
+julia> Dt = differential(t,t)    
+#69 (generic function with 1 method)
+
+julia> Dt(f)
+2
+```
+"""
+function differential(variables::Node...)
+    return (a::Node) -> derivative([a], variables...)[1]
+end
+export differential
