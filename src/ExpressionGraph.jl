e1a67227b4661fd5255af6ce7624c19305655e00
brianguenter <1brianguenter@gmail.com>
2023-11-10 14:46:58 -0800

    fixed bug https://github.com/brianguenter/FastDifferentiation.jl/issues/53#issue-1988279310

--- src/ExpressionGraph.jl
+++ src/ExpressionGraph.jl
@@ -173,7 +173,7 @@ function simplify_check_cache(::typeof(^), a, b, cache)
     if constant_value(na) !== nothing && constant_value(nb) !== nothing
         return Node(constant_value(na)^constant_value(nb))
     elseif value(nb) == 0 #IEEE-754 standard says any number, including Inf, NaN, etc., to the zero power is 1
-        return Node(one(value(b))) #try to preserve number type
+        return Node(one(value(nb))) #try to preserve number type
     elseif value(nb) == 1
         return a
     else
